4c4d67c9c5c5968d1311b091cd05ee7f13efef68
W1ntrr <ryancatt390@gmail.com>
2025-04-21 14:37:31 -0400

    Add CSS for homepage layout

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import "./style.css";
-import { loadAboutPage } from "./about.js";
-import { loadContactPage } from "./contact.js";
-import { loadHomePage } from "./home.js";
+import './style.css';
+import { loadAboutPage } from './pages/about.js';
+import { loadContactPage } from './pages/contact.js';
+import { loadHomePage } from './pages/home.js';
 
-const content = document.getElementById("content");
-const tabButtons = document.querySelectorAll("nav button");
+const content = document.getElementById('content');
+const tabButtons = document.querySelectorAll('nav button');
 
-const tabs = ["Home", "About", "Contact"];
+const tabs = ['Home', 'About', 'Contact'];
 
 loadHomePage();
 initializeTabButtons();
 
 function initializeTabButtons() {
   tabButtons.forEach((tab, index) => {
-    tab.setAttribute("data-tab", tabs[index]);
-    tab.addEventListener("click", () => {
-      const tabId = tab.getAttribute("data-tab");
+    tab.setAttribute('data-tab', tabs[index]);
+    tab.addEventListener('click', () => {
+      const tabId = tab.getAttribute('data-tab');
       switchTab(tabId);
     });
   });
 }
 
 function switchTab(tab) {
-  content.innerHTML = "";
+  content.innerHTML = '';
   switch (tab) {
-    case "Home":
+    case 'Home':
       loadHomePage();
       break;
-    case "About":
+    case 'About':
       loadAboutPage();
       break;
-    case "Contact":
+    case 'Contact':
       loadContactPage();
       break;
     default:
---
+++ src/pages/home.js
@@ -0,0 +1,48 @@
+import heroFood from '../images/food.png';
+import { loadAboutPage } from './about.js';
+
+export function loadHomePage() {
+  const content = document.getElementById('content');
+
+  const hero = document.createElement('section');
+  hero.classList.add('hero');
+
+  const leftSection = document.createElement('div');
+  leftSection.classList.add('left-section');
+
+  const restaurantTitle = document.createElement('h1');
+  restaurantTitle.classList.add('restaurant-title');
+  restaurantTitle.textContent = 'Food';
+
+  const description = document.createElement('p');
+  description.classList.add('description');
+  description.textContent = 'Bold flavors, fresh and fast.';
+
+  const actionContainer = document.createElement('div');
+  actionContainer.classList.add('action-container');
+
+  const learnMoreBtn = document.createElement('button');
+  learnMoreBtn.classList.add('learn-more-btn');
+  learnMoreBtn.textContent = 'Learn More';
+  learnMoreBtn.addEventListener('click', () => {
+    loadAboutPage();
+    content.innerHTML = '';
+  });
+
+  const rightSection = document.createElement('div');
+  rightSection.classList.add('right-section');
+
+  const heroImg = document.createElement('img');
+  heroImg.src = heroFood;
+  heroImg.alt = 'hero';
+  heroImg.classList.add('hero-image');
+
+  content.appendChild(hero);
+  hero.appendChild(leftSection);
+  leftSection.appendChild(restaurantTitle);
+  leftSection.appendChild(description);
+  leftSection.appendChild(actionContainer);
+  actionContainer.appendChild(learnMoreBtn);
+  hero.appendChild(rightSection);
+  rightSection.appendChild(heroImg);
+}
