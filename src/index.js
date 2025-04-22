a6d38e497e1f94849415d2b64dcea5bbe18cfbc3
W1ntrr <ryancatt390@gmail.com>
2025-04-22 16:20:53 -0400

    Cleanup background image

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import './style.css';
 import { loadAboutPage } from './pages/about.js';
 import { loadMenuPage } from './pages/menu.js';
 import { loadHomePage } from './pages/home.js';
-import mainBackground from './images/Food-Background.png';
-import aboutBackground from './images/food-background2.png';
-import menuBackground from './images/food-background3.png';
 
 const content = document.getElementById('content');
 const tabButtons = document.querySelectorAll('nav button');
@@ -17,26 +14,10 @@ title.classList.add('title');
 title.innerHTML = `Food <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M416 0C400 0 288 32 288 176l0 112c0 35.3 28.7 64 64 64l32 0 0 128c0 17.7 14.3 32 32 32s32-14.3 32-32l0-128 0-112 0-208c0-17.7-14.3-32-32-32zM64 16C64 7.8 57.9 1 49.7 .1S34.2 4.6 32.4 12.5L2.1 148.8C.7 155.1 0 161.5 0 167.9c0 45.9 35.1 83.6 80 87.7L80 480c0 17.7 14.3 32 32 32s32-14.3 32-32l0-224.4c44.9-4.1 80-41.8 80-87.7c0-6.4-.7-12.8-2.1-19.1L191.6 12.5c-1.8-8-9.3-13.3-17.4-12.4S160 7.8 160 16l0 134.2c0 5.4-4.4 9.8-9.8 9.8c-5.1 0-9.3-3.9-9.8-9L127.9 14.6C127.2 6.3 120.3 0 112 0s-15.2 6.3-15.9 14.6L83.7 151c-.5 5.1-4.7 9-9.8 9c-5.4 0-9.8-4.4-9.8-9.8L64 16zm48.3 152l-.3 0-.3 0 .3-.7 .3 .7z"/></svg>`;
 
 const tabs = ['Home', 'Menu', 'About'];
-const backgroundImages = {
-  Home: mainBackground,
-  Menu: menuBackground,
-  About: aboutBackground,
-};
 
 loadHomePage();
 initializeTabButtons();
 animateHero();
-setBackground(mainBackground);
-
-function setBackground(image) {
-  document.body.style.backgroundImage = `linear-gradient(
-    rgba(27, 27, 27, 0.849),
-    rgba(22, 22, 22, 0.726)
-  ), url(${image})`;
-  document.body.style.backgroundPosition = 'center';
-  document.body.style.backgroundSize = 'cover';
-  document.body.style.backgroundRepeat = 'no-repeat';
-}
 
 function animateHero() {
   const hero = document.querySelector('.hero');
@@ -64,7 +45,6 @@ function switchTab(tab) {
     case 'Home':
       loadHomePage();
       animateHero();
-      setBackground(backgroundImages.Home);
       break;
     case 'Menu':
       loadMenuPage();
